87305eb4b3bcce396d84c901fe568c94e2c01358
Jose Daniel Rodriguez Sotela <61768197+Enderguy747@users.noreply.github.com>
2022-04-22 23:14:39 -0600

    second commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -6,7 +6,7 @@ const Loading = () => {
       <svg className='spinner' width='65px' height='65px' viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
         <circle class='path' fill='none' stroke-width='6' stroke-linecap='round' cx='33' cy='33' r='30' />
       </svg>
-      <h3>Loading...</h3>
+      <h3>Loading....</h3>
     </div>
   )
 }
